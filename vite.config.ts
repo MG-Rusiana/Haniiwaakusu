20eaf034b7a9e08a45ffad2b9ece9ae2452474bc
Mark Gil P. Rusiana <markgilrusiana437@gmail.com>
2024-03-06 11:42:08 +0800

    setting up a tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/haniiwaakusu/",
+  base: "/Haniiwaakusu/",
   plugins: [react()],
 })
