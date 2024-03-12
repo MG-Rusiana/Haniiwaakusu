4646c80a5c5b787efab7ec0087275e84b607a718
Mark Gil P. Rusiana <markgilrusiana437@gmail.com>
2024-03-12 09:43:10 +0800

    Updates

---
+++ assets/reactTable-DjJ57pJ8.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-pXw5faXT.js";function s(){return e.jsx("div",{className:" w-screen h-screen flex justify-center items-center ",children:e.jsx("h1",{children:"React Table Testing"})})}export{s as default};
